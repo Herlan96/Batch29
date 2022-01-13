d9aaf06a428c3edd0df3dcc99bd044f8ae49baf6
herlan <apriantoherlan@gmail.com>
2022-01-13 14:53:53 +0700

    fix code

---
+++ challange2-1.js
@@ -0,0 +1,12 @@
+function deretKaskus(n){
+    const nama = Kas
+    const nama = Kus
+    for (let i=3; i < n; i++)
+    kaskus === deretKaskus[i];
+      return console.log(deretKaskus);
+
+}
+
+
+    console.log(deretKaskus(10))
+
---
+++ challenge2.js
@@ -0,0 +1,7 @@
+function deretKaskus(n){
+if (n % 10 > 0){
+    return 
+}
+}
+
+console.log(deretKaskus(10));
