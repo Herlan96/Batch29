6b3ad58c1840993a7b70b50edf0abff444dff6a2
Herlan <97658232+Herlan96@users.noreply.github.com>
2022-01-14 05:22:25 +0700

    Add files via upload

---
+++ challenge5.js
@@ -0,0 +1,13 @@
+function stringManipulation(word) {
+    
+    let newword = word.split("")
+
+    if (newword[0] === "a" || newword[0] === "i" || newword[0] === "u" || newword[0] === "e" || newword[0] === "o") {
+        console.log(word)
+    }
+    else console.log(word.slice(1, word.length) + word.charAt(0) + 'nyo')
+}
+
+
+stringManipulation('ayam')
+stringManipulation('bebek')
