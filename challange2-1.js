47f82128e7d33223c586cd8609488e512e813b33
Herlan <97658232+Herlan96@users.noreply.github.com>
2022-01-13 17:57:18 +0700

    Add files via upload

---
+++ challange2-1.js
@@ -0,0 +1,16 @@
+function deretKaskus(n) {
+  let hasil = [];
+  for (let i=3; i <= n*3; i+=3) {
+    if(i % 6 == 0 && i % 5 == 0)hasil.push('KASKUS')
+     else if(i % 5 == 0) hasil.push('KAS')
+     else if(i % 6 == 0) hasil.push('KUS')
+     else {
+      hasil.push(i)
+    }
+    
+  }
+  return hasil
+}
+  console.log(deretKaskus(10))
+
+
