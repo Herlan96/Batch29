2a624dd1ab788af19e8d3362b027c760394a74ce
herlan <apriantoherlan@gmail.com>
2022-01-19 07:33:16 +0700

    challenge.js

--- challenge7.js
+++ challenge7.js
@@ -1,13 +1,21 @@
 function weirdMultiply(sentence) {
 
     let str = sentence.toString()
-    let total = 1
-  for (let i=0; i < str.length; i++) {
-    total *= str[i]
     
-    }return total
+    if(str.length>1){
+
+    let total = 1
+
+    for (let i=0; i < str.length; i++) {
+    total *= str[i]
+    }
+        return weirdMultiply(total)
+    }else{
+        return sentence
+    }
 }
 
 console.log(weirdMultiply(39))
 console.log(weirdMultiply(999))
 console.log(weirdMultiply(3))
+
