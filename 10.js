ca57ae78fdabab0d7414a888988dbb702d7264f4
herlan <apriantoherlan@gmail.com>
2022-01-25 16:40:23 +0700

    challenge.js

--- 10.js
+++ 10.js
@@ -12,9 +12,28 @@ function sentenceManipulation(sentence) {
             + newarray[i].charAt(0) + 'nyo')
         }
 
-    }console.log(result.join(' '))
-    
+    } return result.join(' ')
+
 }
-    
-    
-sentenceManipulation('ibu pergi ke pasar bersama aku')
+
+
+
+
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'tulis kalimatmu disini >'
+});
+
+rl.prompt();
+
+rl.on('line', (line) => {
+    console.log(sentenceManipulation(line));
+
+    rl.prompt();
+}).on('close', () => {
+    console.log('Good bye!');
+    process.exit(0);
+});
