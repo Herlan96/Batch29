fb09f6420e5826d024c8be7aff78f3d26968b548
herlan <apriantoherlan@gmail.com>
2022-01-25 18:12:59 +0700

    challenge.js

---
+++ c11/tebakkata.mjs
@@ -0,0 +1,37 @@
+// const raedline = require('readline')
+import readline from 'readline'
+import fs from 'fs'
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Tebakan :"
+})
+console.log("Selamat datang di permainan Tebak Kata, silahkan isi dengan jawaban yang benar ya!")
+
+var soal = JSON.parse(fs.readFileSync('data.json', 'utf-8'))
+let index = 0
+
+console.log(soal[index].definition)
+
+rl.prompt();
+
+rl.on('line', (answer) => {
+    if (answer == soal[index].term) {
+        console.log("Selamat Anda Benar!")
+        index++
+        if (index < soal.length)
+            console.log(soal[index].definition)
+        else {
+            console.log('Hore Anda Menang!')
+            rl.close()
+        }
+    } else {
+        console.log("Anda Belum Beruntung!")
+    }
+
+
+    rl.prompt();
+}).on(`close`, () => {
+    process.exit(0);
+});
