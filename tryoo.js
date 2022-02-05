7f54968e914da2fc06f90b4bfca4e805eb91954c
herlan <apriantoherlan@gmail.com>
2022-02-05 22:43:18 +0700

    tryout.js

---
+++ tryoo.js
@@ -0,0 +1,11 @@
+const array = [2, 5, 9];
+
+console.log(array);
+
+const index = array.indexOf(5);
+if (index > -1) {
+  array.splice(index, 1); // 2nd parameter means remove one item only
+}
+
+
+console.log(array);
