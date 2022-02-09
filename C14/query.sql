9c17dcb47a428605e78c2f10311bde05b17c141c
herlan <apriantoherlan@gmail.com>
2022-02-09 16:00:54 +0700

    challenge.js

--- C14/query.sql
+++ C14/query.sql
@@ -1,24 +1,47 @@
-SQLite3 University.db
+SQLite3 university.db
+create table jurusan(
+kodejurusan varchar primary key not null, 
+namajurusan varchar not null
+);
 
-create table jurusan(kodejurusan varchar primary key not null, namajurusan varchar not null);
-insert into jurusan(kodejurusan varchar primary key not null, namajurusan varchar) values ('61201', 'Manajemen');--menambahkan
+insert into jurusan(kodejurusan, namajurusan) values ('61201', 'manajemen');--menambahkan
 
-create table Mahasiswa(Nim varchar primary key not null, Name varchar, Alamat varchar, jurusan varchar, FOREIGN KEY(jurusan) REFERENCES jurusan(kodejurusan));
-insert into Mahasiswa(Nim,Nama,Alamat,Jurusan) values ('143402182', 'Herlan', 'Tasik', 'Manajemen');
+create table mahasiswa(
+nim varchar primary key not null, 
+nama varchar,
+alamat varchar, 
+jurusan varchar, 
+FOREIGN KEY(jurusan) REFERENCES jurusan(kodejurusan)
+);
 
-create table dosen(kodedosen varchar primary key not null,nama varchar);
-insert into dosen(kodedosen, nama)values('d1', 'rubi');
+insert into mahasiswa(nim, nama, alamat, jurusan) values ('143402182', 'herlan', 'tasik', '61201');
 
-create table matakuliah(kodematkul varchar primary key not null, nama varchar, Sks integer);
-insert into matakuliah(kodematkul, nama, Sks)values('01', 'rubi', '3');
+create table dosen(
+kodedosen varchar primary key not null,
+nama varchar
+);
+
+insert into dosen(kodedosen, nama) values ('d1', 'rubi');
+
+create table matakuliah(
+kodematkul varchar primary key not null, 
+nama varchar, 
+sks integer
+);
+
+insert into matakuliah(kodematkul, nama, sks) values ('m1', 'rubi', '3');
 
 -- foreign key ada key yang ada di setiap tabel
 
-create table kontrak(id integer primary key autoincrement, 
-    nim varchar null, 
-    kodematkul varchar not null, 
-    kodedosen varchar not null,
-    nilai varchar,
-    FOREIGN KEY(nim) REFERENCES mahasiswa(nim)--key nim yang terdapat mahasiswa dengan nim
-    FOREIGN KEY(kodematkul) REFERENCES matakuliah(kodematkul)
-    FOREIGN KEY(kodedosen) REFERENCES dosen(kodedosen));
+create table kontrak(
+id integer primary key autoincrement, 
+nim varchar not null, 
+kodematkul varchar not null, 
+kodedosen varchar not null,
+nilai varchar,
+FOREIGN KEY(nim) REFERENCES mahasiswa(nim)--key nim yang terdapat mahasiswa dengan nim
+FOREIGN KEY(kodematkul) REFERENCES matakuliah(kodematkul)
+FOREIGN KEY(kodedosen) REFERENCES dosen(kodedosen)
+);
+
+insert into kontrak(nim, kodematkul, kodedosen) values ('143402182', 'm1', 'd1');
