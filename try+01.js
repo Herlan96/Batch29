a23981af4f38b057e3f9d9409297d48e095dd52a
herlan <aoriantoherlan@gmail.com>
2022-02-16 16:31:45 +0700

    TRYOUT

--- try+01.js
+++ try+01.js
@@ -1,14 +1,14 @@
 const items = [1, 2, 3, 4, 5]
 
-const insert = (arr, index, newItem) => [
+const insert = (arr, index, ...newItems) => [
   // part of the array before the specified index
   ...arr.slice(0, index),
-  // inserted item
-  newItem,
+  // inserted items
+  ...newItems,
   // part of the array after the specified index
   ...arr.slice(index)
 ]
 
-const result = insert(items, 1, 10)
+const result = insert(items, 1, 10, 20)
 
 console.log(result)
